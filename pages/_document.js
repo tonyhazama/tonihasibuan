9ab5873eaeb8c94fc10ccd15c219793f9bef989c
Tony Hazama <tonyhazama4721@gmail.com>
2022-04-24 20:01:33 +0700

    added smooth scrolling

--- pages/_document.js
+++ pages/_document.js
@@ -3,7 +3,7 @@ import Document, { Html, Head, Main, NextScript } from "next/document";
 export default class MyDocument extends Document {
   render() {
     return (
-      <Html lang="en">
+      <Html lang="en" style={{scrollBehavior: 'smooth'}}>
         <Head />
         <body>
           <Main />
